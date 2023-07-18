e27f03ae12f81cdda029685f73d5b45613dc31de
Hari-1202 <hariharandhanraj0203@gmail.com>
2023-07-18 20:39:43 +0530

    Enquiry alert message added

--- components/Header.js
+++ components/Header.js
@@ -3,28 +3,29 @@ import { useState } from 'react'
 import Enquire from './Enquire'
 const Header = ({ title }) => {
     const [enquired, setEnquired] = useState(false)
-    // const [enquiredStatus, setEnquiredStatus] = useState(false)
+    const [enquiredStatus, setEnquiredStatus] = useState(false)
     const headerValues = ["home", "about us", "residential", "commercial", "joint venture", "media center"]
 
     const onEnquire = (value) => {
         setEnquired(value)
-        // setEnquiredStatus(true)
-        // setTimeout(() => {
-        //     setEnquiredStatus(false)
-        // }, 2000)
+        setEnquiredStatus(true)
+        setTimeout(() => {
+            setEnquiredStatus(false)
+        }, 10000)
     }
 
 
     console.log({ enquiredStatus })
     return (
         <>
+            {console.log("rerendered")}
             <div className=' bg-yellow-600'>
                 <h1 className='header text-m text-green-600 uppercase font-bold text-center text-xl p-1' >Real Estate Builders</h1>
+                {enquiredStatus && <div className='bg-white w-80 alert' ><h1 className='text-center font-bold p-4 '>Thanks for reaching out . We will get in touch shortly !</h1></div>}
                 <div className='group wrapper'>
                     <h1 className='header text-m text-blue-600 text-center p-4 font-bold' >Enquire now</h1>
                     <div className='hidden group-hover:flex  justify-center'>
                         {!enquired ? <Enquire enquired={enquired} onEnquire={onEnquire} /> : <div className='bg-white'><h1 className='text-green-700 p-4'>Already enquired . Please wait until our team reaches out to you</h1></div>}
-                        {/* {enquiredStatus && <h1>Thanks for reaching out . We will get in touch shortly !</h1>} */}
                     </div>
                 </div>
                 <div className='flex'>
