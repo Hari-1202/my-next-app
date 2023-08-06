411c9a5e95263a23690749b96876cdb66636e950
Hari-1202 <hariharandhanraj0203@gmail.com>
2023-08-06 20:37:21 +0530

    Hiding current skillCategory when different one is selected

--- components/Nodes.js
+++ components/Nodes.js
@@ -1,33 +1,54 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const Nodes = ({ skillInfo: { skillNode: skillSet } }) => {
     const [skills, setSkills] = useState(skillSet)
-    useEffect(() => {
-        console.log(skills)
-    }, [])
     const [showingSkills, setShowingSkills] = useState({})
     const [showingLang, setShowingLang] = useState({})
     const [showingfw, setShowingfw] = useState({})
-
+    const [currentLanguage, setCurrentLanguage] = useState('')
+    const [currentCategory, setCurrentCategory] = useState('')
     const onClick = ({ category, lang = '', framework = '' }) => {
         if (lang !== '') {
-            setShowingLang(showingSkills[lang]);
+            if (Object.keys(showingLang).length > 0 && currentLanguage === lang) {
+                setCurrentLanguage(lang)
+                setShowingLang({})
+                setShowingfw({})
+            } else {
+                setCurrentLanguage(lang)
+                setShowingLang(showingSkills[lang]);
+            }
         } else if (framework !== '') {
-            console.log(showingLang, framework);
-            if (showingLang.hasOwnProperty(framework)) {
-                if (showingLang[framework].hasOwnProperty("frameworks")) {
-                    setShowingfw({ ...showingfw, [framework]: showingLang[framework]["frameworks"] })
-                } else {
-                    setShowingfw({ ...showingfw, [framework]: showingLang[framework] })
+            if (showingfw.hasOwnProperty(framework)) {
+                const filteredObj = { ...showingfw }
+                const findFramework = (frameworkVal) => frameworkVal === framework
+                let index = Object.keys(filteredObj).findIndex(findFramework)
+                Object.entries(filteredObj).slice(index, Object.keys(filteredObj).length).map((items) => delete filteredObj[items[0]])
+                setShowingfw(filteredObj)
+            } else {
+                if (showingLang.hasOwnProperty(framework)) {
+                    if (showingLang[framework].hasOwnProperty("frameworks")) {
+                        setShowingfw({ ...showingfw, [framework]: showingLang[framework]["frameworks"] })
+                    } else {
+                        setShowingfw({ ...showingfw, [framework]: showingLang[framework] })
+                    }
                 }
             }
         }
         else {
-            let showingSkills = skills[category]
-            setShowingSkills(showingSkills)
+            if (Object.keys(showingSkills).length > 0 && category === currentCategory) {
+                setCurrentCategory(category)
+                setShowingSkills({})
+                setShowingLang({})
+                setShowingfw({})
+            } else {
+                let showingSkills = skills[category]
+                setCurrentCategory(category)
+                setShowingSkills(showingSkills)
+                setShowingLang({})
+                setShowingfw({})
+            }
         }
     }
-    console.log(showingfw)
     return (
         <div>
             {Object.keys(skills).map((category) => {
